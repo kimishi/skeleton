2557af415a88ad6b15ed9ca2cfe429171d5fda5c
Kimi Shi <kimishi1983@gmail.com>
2014-12-22 23:42:18 +0800

    Initialize skeleton

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = [
+    'description' : 'My Project',
+    'author' : 'Kimi Shi',
+    'url' : 'URL to get it at.',
+    'download_url' : 'Where to download it.',
+    'version' : '0.1',
+    'install_requires' : ['nose'],
+    'packages' : ['NAME'],
+    'scripts' : [],
+    'name' : 'projectname'
+]
+
+setup(**config)
